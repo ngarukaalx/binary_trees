b0981b35248d520244af5904142092e64b82be06
ngarukaalx <hiramkabuagi@gmail.com>
2023-11-02 16:28:50 +0000

    sibling

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -40,6 +42,4 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 #endif /* _BINARY_TREES_H_ */
